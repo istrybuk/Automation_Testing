4dbfb4545f5c5b6ea11d87dd95f6d14613d57191
Strybuk Ivan <79363807+istrybuk@users.noreply.github.com>
2023-11-08 11:40:24 +0300

    Delete Test_UI/demoqa.com directory

--- Test_UI/demoqa.com/config/links.py
+++
@@ -1,4 +0,0 @@
-class Links:
-
-    HOST = "https://demoqa.com/"
-    TEXT_BOX_PAGE = f"{HOST}text-box"
--- Test_UI/demoqa.com/config/links/link_text_box.py
+++
@@ -1,7 +0,0 @@
-import config.links.link_main_page as loc
-
-
-class LinkTextBoxPage:
-    """Links to text box pages"""
-
-    TEXT_BOX_PAGE = f"{loc.LINK_HOST}text-box"
